971a7171cafa124431773358d3fb6e2ffc089d61
vitalwer <66179292+vitalwer@users.noreply.github.com>
2020-07-04 15:53:53 +0300

    first commit

---
+++ Academy/а/for.js
@@ -0,0 +1,12 @@
+/*var result = '';
+var i = 100;
+do {
+  i += 20;
+  result += i + ' ';
+} while (i < 8);
+document.write(result);*/
+
+for (var i = 100; i < 60; i -- 20) {
+ 
+}
+document.write(result);
---
+++ script.js
@@ -0,0 +1,14 @@
+var buttonPlus = document.getElementById('buttonPlus');
+var buttonMinus = document.getElementById('buttonMinus');
+function onButtonPlusClick() {
+  var num1,num2, result;
+  num1=Number(document.getElementById('n1').value);
+  num2=Number(document.getElementById('n2').value);
+  result=num1+num2;
+  console.log(result);
+}
+function onButtonMinusClick() {
+    console.log('onButtonMinusClick');
+  }
+buttonPlus.addEventListener('click', onButtonPlusClick);
+buttonMinus.addEventListener('click', onButtonMinusClick);
